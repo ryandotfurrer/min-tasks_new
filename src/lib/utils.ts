6550dd1a887304b1990173e50e4e081d78004f83
Ryan Furrer <ryandotfurrer@gmail.com>
2025-03-06 13:54:47 -0500

    chore: update package-lock.json and clean up utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-import { getXataClient } from "@/xata";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
-
-const xata = getXataClient();
-
-const newTask = await xata.db.tasks.create({
-  xata_id: "id-40232288",
-  taskCompleted: true,
-  taskName: "longer text",
-});
-console.log(newTask);
